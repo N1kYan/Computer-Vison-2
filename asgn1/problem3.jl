5e97846711ecf34be576862511cdb42f5014124d
N1kYan <yannik_phil.frisch@stud.tu-darmstadt.de>
2019-05-07 18:00:40 +0200

    finished problem 3

--- asgn1/problem3.jl
+++ asgn1/problem3.jl
@@ -1,15 +1,12 @@
-# Assignment 1
+# Assignment 1 - Exercise 3
 import FileIO
-using Plots, Colors
-pyplot()
+using PyPlot
 
 
-# Exercise 3
 print("Exercise 3:\n")
 # Read Image
 img_path = string(@__DIR__,"/skeleton/a1p3.png")
 img = FileIO.load(img_path)
 # Convert to Float64 grayscale
 gray_img = Gray.(img)
-figure()
 plot(gray_img)
