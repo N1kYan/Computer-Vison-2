12906ad873653332385ad29a1cb66492d80c5239
N1kYan <yannik_phil.frisch@stud.tu-darmstadt.de>
2019-05-27 23:57:00 +0200

    starting asgn2

--- asgn2/problem2.jl
+++ asgn2/problem2.jl
@@ -2,8 +2,16 @@ using PyPlot
 
 
 
-# Load Tsukuba disparity dataset
+# Load Tsukuba disparity dataset and convert it to grayscale
 function load_data()
+    i0_path = string(@__DIR__,"/i0.png")
+    i0 = imread(i0_path)
+    i0 = convert_to_grayscale(i0)
+    i1_path = string(@__DIR__,"/i1.png")
+    i1 = imread(i1_path)
+    i1 = convert_to_grayscale(i1)
+    gt_path = string(@__DIR__,"/gt.png")
+    gt64 = convert(Array{Float64,2}, imread(gt_path)*255)
 
     @assert maximum(gt) <= 16
     return i0::Array{Float64,2}, i1::Array{Float64,2}, gt::Array{Float64,2}
@@ -30,7 +38,9 @@ end
 # Evaluate log of Student-t distribution.
 # Set sigma=0.7 and alpha=0.8
 function log_studentt(x::Array{Float64,2})
-
+    sigma = 0.7
+    alpha = 0.8
+    value = (1 + (1 / 2*sigma^2)*d^2)^(-alpha)
 
     return value::Array{Float64,2}
 end
@@ -60,4 +70,3 @@ function problem2()
 
 
 end
-
